dc046d75b01d702ce3a62349196de0815f2dd07f
blinke <Burkhard.Linke@computational.bio.uni-giessen.de>
2019-02-08 14:57:06 +0100

    removed broken threading (we need real error handling...) fixed handling of service within gunicorn

--- denbi/scripts/perun_propagation_service.py
+++ denbi/scripts/perun_propagation_service.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-import argparse
 import logging
 import os
 import shutil
 import tarfile
 import tempfile
 
+from datetime import datetime
 from flask import Flask
 from flask import request
 from denbi.perun.endpoint import Endpoint
 from denbi.perun.keystone import KeyStone
 from threading import Thread
+
 
 app = Flask(__name__)
 app.config['cleanup'] = True
@@ -18,28 +19,32 @@ app.config['keystone_read_only'] = os.environ.get('KEYSTONE_READ_ONLY', 'False')
 logging.basicConfig(level=getattr(logging, os.environ.get('PERUN_LOG_LEVEL', 'WARN')))
 
 
-def process_tarball(tarball_path, read_only=False, target_domain_name='elixir',
-                    default_role='user', nested=False, support_quota=False):
+def process_tarball(tarball_path, base_dir=tempfile.mkdtemp(), read_only=False, target_domain_name='elixir',
+                    default_role='user', nested=False, support_quota=False, cloud_admin=True):
     # TODO(hxr): deduplicate
-    directory = tempfile.mkdtemp()
+    d = datetime.today()
+    dir = base_dir + "/" + str(d.year) + "_" + str(d.month) + "_" + str(d.day) + "-" + str(d.hour) + ":" + str(d.minute) + ":" + str(d.second) + "." + str(d.microsecond)
+    os.mkdir(dir)
+
     logging.info("Processing data uploaded by Perun: %s" % tarball_path)
 
     # extract tar file
     tar = tarfile.open(tarball_path, "r:gz")
-    tar.extractall(path=directory)
+    tar.extractall(path=dir)
     tar.close()
 
     # import into keystone
-    keystone = KeyStone(default_role=default_role, create_default_role=True,
+    keystone = KeyStone(environ=app.config, default_role=default_role, create_default_role=True,
                         target_domain_name=target_domain_name,
-                        read_only=read_only, nested=nested)
+                        read_only=read_only, nested=nested, cloud_admin=cloud_admin)
+
     endpoint = Endpoint(keystone=keystone, mode="denbi_portal_compute_center",
                         support_quotas=support_quota)
-    endpoint.import_data(directory + '/users.scim', directory + '/groups.scim')
+    endpoint.import_data(dir + '/users.scim', dir + '/groups.scim')
     logging.info("Finished processing %s" % tarball_path)
 
     # Cleanup
-    shutil.rmtree(directory)
+    shutil.rmtree(dir)
 
 
 @app.route("/upload", methods=['PUT'])
@@ -53,50 +58,33 @@ def upload():
     # store uploaded data
     file.write(request.get_data())
     file.close()
-
     # parse propagated data in separate thread
-    t = Thread(target=_perun_propagation, args=(file.name,),
-               kwargs={'read_only': app.config.get('keystone_read_only', False),
-                       'target_domain_name': app.confg.get('target_domain_name', 'elixir'),
-                       'default_role': app.config.get('default_role', 'user'),
-                       'nested': app.config.get('nested'),
-                       'support_quota': app.config.get('support_quota')})
-    t.start()
+#    t = Thread(target=_perun_propagation, args=(file.name,),
+#               kwargs={'read_only': app.config.get('KEYSTONE_READ_ONLY', False),
+#                       'target_domain_name': app.config.get('TARGET_DOMAIN_NAME', 'elixir'),
+#                       'default_role': app.config.get('DEFAULT_ROLE', 'user'),
+#                       'nested': app.config.get('NESTED', False),
+#                       'cloud_admin': app.config.get('CLOUD_ADMIN', True),
+#                       'base_dir': app.config.get('BASE_DIR', tempfile.mkdtemp()),
+#                       'support_quota': app.config.get('SUPPORT_QUOTA', False)})
+#    t.start()
 
-    # return immediately
+    process_tarball(file.name, read_only=app.config.get('KEYSTONE_READ_ONLY', False),
+                    target_domain_name=app.config.get('TARGET_DOMAIN_NAME', 'elixir'),
+                    default_role=app.config.get('DEFAULT_ROLE', 'user'),
+                    nested=app.config.get('NESTED', False),
+                    cloud_admin=app.config.get('CLOUD_ADMIN', True),
+                    base_dir=app.config.get('BASE_DIR', tempfile.mkdtemp()),
+                    support_quota=app.config.get('SUPPORT_QUOTA', False))
+
+    if app.config.get('CLEANUP', False):
+      os.unlink(file)
+
+
+
     return ""
 
 
-def _perun_propagation(file, read_only=False, target_domain_name="elixir"):
-    process_tarball(file, read_only=read_only, target_domain_name=target_domain_name)
-
-    if app.config['cleanup']:
-        os.unlink(file)
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Run perunKeystoneAdapter service')
-    parser.add_argument('--host', default='0.0.0.0', help="Address to bind to")
-    parser.add_argument('--port', type=int, default=5000, help="Port to bind to")
-    parser.add_argument('--read-only', action='store_true', help="Do not make any modifications to keystone")
-    parser.add_argument('--domain', default='elixir',
-                        help="Domain to create users and projects in, defaults to 'elixir'")
-    parser.add_argument('--role', default='user',
-                        help="Defaut role to assign to new users, defaults to 'user'")
-    parser.add_argument("-n", "--nested", action="store_true", default=False,
-                        help="use nested project instead of cloud/domain admin")
-    parser.add_argument("-q", "--quotas", action="store_true", default=False,
-                        help="set quotas for projects")
-    args = parser.parse_args()
-
-    app.config['keystone_read_only'] = args.read_only
-    app.config['target_domain_name'] = args.domain
-    app.config['default_role'] = args.role
-    app.config['nested'] = args.nested
-    app.config['support_quota'] = args.support_quota
-
-    app.run(host=args.host, port=args.port)
-
-
+app.config.from_envvar('CONFIG_PATH')
 if __name__ == "__main__":
-    main()
+  app.run(host=app.config['HOST'], port=app.config['PORT'])
